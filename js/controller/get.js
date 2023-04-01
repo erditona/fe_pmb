a9ee3c30017e67638a25904889bfeb9f6635129d
erditona <erditonaushaadam@gmail.com>
2023-04-01 11:35:51 +0700

    merapikan

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,7 +1,7 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTable } from "../temp/table.js";
-export function isiTablePresensi(results) {
+export function isiTablePendaftaran(results) {
   results.forEach(isiRow);
 }
 function isiRow(value) {
