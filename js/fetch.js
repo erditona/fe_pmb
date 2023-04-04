588363191765c660ee0b206549ff5754bdd2dd47
erditona <erditonaushaadam@gmail.com>
2023-04-04 18:56:07 +0700

    update-fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,6 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { isiTablePendaftaran } from "./controller/get.js";
-import { isiTableJurusan } from "./controller/get.js";
-import { urlAPI } from "./config/url.js";
-import { urlAPIJurusan } from "./config/url.js";
+import { isiTablePendaftaran, isiTableJurusan } from "./controller/get.js";
+import { urlAPI, urlAPIJurusan } from "./config/url.js";
+
 get(urlAPI, isiTablePendaftaran);
 get(urlAPIJurusan, isiTableJurusan);
