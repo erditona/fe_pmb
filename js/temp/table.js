a89291117e2188984c61a7f04524dd931f9827b4
erditona <erditonaushaadam@gmail.com>
2023-04-04 20:06:22 +0700

    test

--- js/temp/table.js
+++ js/temp/table.js
@@ -60,12 +60,7 @@ export let isiTable2 = `
         </div>
       </div>
     </th>
-    <th class="whitespace-nowrap px-4 bg-white text-center">
-        <div class="w-auto p-2">
-          <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
-        </div>
-      </div>
-    </th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-center">#NAMA#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-center">#JENJANG#</th>
     <th class="whitespace-nowrap pr-4 bg-white text-sm font-medium text-coolGray-800">
         <svg class="ml-auto" width="16" height="16" viewbox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
