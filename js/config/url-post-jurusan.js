78bc0610930b555719e6195d2f335e39a45834f2
erditona <erditonaushaadam@gmail.com>
2023-06-09 16:58:36 +0700

    test

--- js/config/url-post-jurusan.js
+++ js/config/url-post-jurusan.js
@@ -3,5 +3,5 @@ export let urlPOST = "https://ws-dito.herokuapp.com/insrt-jurusan";
 export function AmbilResponse(result) {
   console.log(result); //menampilkan response API pada console
   alert(result.message); //menampilkan response API pada alert
-  window.location.reload(); //reload halaman setelah klik ok pada alert
+  //   window.location.reload(); //reload halaman setelah klik ok pada alert
 }
