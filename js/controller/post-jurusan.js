aaa7f49077288ab991cf2369d13c46a7a11784b8
erditona <erditonaushaadam@gmail.com>
2023-06-16 08:44:19 +0700

    tes

--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -2,38 +2,43 @@ import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url-post-jurusan.js";
 
+function pushData() {
+  if (kadjurusanValue === "" || namaValue === "" || jenjangValue === "") {
+    console.log("Please fill in all fields");
+    return; // Stop further execution if the form is not valid
+  } else {
+    let data = {
+      kdjurusan: getValue("kdjurusan"),
+      nama: getValue("nama"),
+      jenjang: getValue("jenjang"),
+    };
+    console.log(data);
+    postData(urlPOST, data, AmbilResponse);
+  }
+}
+
+onClick("button", pushData);
+
 // function pushData() {
+//   // Get the form values
+//   let kadjurusanValue = getValue("kdjurusan");
+//   let namaValue = getValue("nama");
+//   let jenjangValue = getValue("jenjang");
+
+//   // Perform form validation
+//   if (kadjurusanValue === "" || namaValue === "" || jenjangValue === "") {
+//     console.log("Please fill in all fields");
+//     return; // Stop further execution if the form is not valid
+//   }
+
 //   let data = {
-//     kdjurusan: getValue("kdjurusan"),
-//     nama: getValue("nama"),
-//     jenjang: getValue("jenjang"),
+//     kdjurusan: kadjurusanValue,
+//     nama: namaValue,
+//     jenjang: jenjangValue,
 //   };
+
 //   console.log(data);
 //   postData(urlPOST, data, AmbilResponse);
 // }
 
 // onClick("button", pushData);
-
-function pushData() {
-  // Get the form values
-  let kadjurusanValue = getValue("kdjurusan");
-  let namaValue = getValue("nama");
-  let jenjangValue = getValue("jenjang");
-
-  // Perform form validation
-  if (kadjurusanValue === "" || namaValue === "" || jenjangValue === "") {
-    console.log("Please fill in all fields");
-    return; // Stop further execution if the form is not valid
-  }
-
-  let data = {
-    kdjurusan: kadjurusanValue,
-    nama: namaValue,
-    jenjang: jenjangValue,
-  };
-
-  console.log(data);
-  postData(urlPOST, data, AmbilResponse);
-}
-
-onClick("button", pushData);
