0fa989c09d2331806e4df97b323dfcbe480aad68
erditona <erditonaushaadam@gmail.com>
2023-06-16 17:35:26 +0700

    mojang

--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -9,33 +9,40 @@ function pushData() {
   let jenjangValue = getValue("jenjang");
 
   // Perform form validation
-  let cek = true;
-  // if ((namaValue === "") | (kadjurusanValue === "") | (jenjangValue === "")) {
-  //   document.getElementById("status").innerHTML = "Data Tidak Boleh Kosong!";
-  //   return;
+  if ((namaValue === "") & (kadjurusanValue === "") & (jenjangValue === "")) {
+    document.getElementById("status").innerHTML = "Data Tidak Boleh Kosong!";
+    return;
+  }
+  // if (namaValue === "") {
+  //   document.getElementById("status").innerHTML = "Nama Tidak Boleh Kosong!";
+  //   cek = false;
   // }
+  // if (kadjurusanValue === "") {
+  //   document.getElementById("status").innerHTML = "Jurusan Tidak Boleh Kosong!";
+  //   cek = false; // Stop further execution if the form is not valid
+  // }
+  // if (jenjangValue === "") {
+  //   document.getElementById("status").innerHTML = "Jenjang Tidak Boleh Kosong!";
+  //   cek = false; // Stop further execution if the form is not valid
+  // }
+
   if (namaValue === "") {
     document.getElementById("status").innerHTML = "Nama Tidak Boleh Kosong!";
-    cek = false;
+    return;
   }
-  if (kadjurusanValue === "") {
-    document.getElementById("status").innerHTML = "Jurusan Tidak Boleh Kosong!";
-    cek = false; // Stop further execution if the form is not valid
-  }
-  if (jenjangValue === "") {
-    document.getElementById("status").innerHTML = "Jenjang Tidak Boleh Kosong!";
-    cek = false; // Stop further execution if the form is not valid
-  }
-  if (cek) {
-    let data = {
-      kdjurusan: kadjurusanValue,
-      nama: namaValue,
-      jenjang: jenjangValue,
-    };
-
-    console.log(data);
-    postData(urlPOST, data, AmbilResponse);
+  if (kadjurusanValue === "" || jenjangValue === "") {
+    document.getElementById("status").innerHTML = "Tolong Lengkapi Semua Kolom!";
+    return; // Stop further execution if the form is not valid
   }
 
-  onClick("button", pushData);
+  let data = {
+    kdjurusan: kadjurusanValue,
+    nama: namaValue,
+    jenjang: jenjangValue,
+  };
+
+  console.log(data);
+  postData(urlPOST, data, AmbilResponse);
 }
+
+onClick("button", pushData);
