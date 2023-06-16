98747eff306bda4d1af4e3392ab1a121ef936fbc
erditona <erditonaushaadam@gmail.com>
2023-06-16 17:40:19 +0700

    fix

--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -13,27 +13,27 @@ function pushData() {
     document.getElementById("status").innerHTML = "Data Tidak Boleh Kosong!";
     return;
   }
-  // if (namaValue === "") {
-  //   document.getElementById("status").innerHTML = "Nama Tidak Boleh Kosong!";
-  //   cek = false;
-  // }
-  // if (kadjurusanValue === "") {
-  //   document.getElementById("status").innerHTML = "Jurusan Tidak Boleh Kosong!";
-  //   cek = false; // Stop further execution if the form is not valid
-  // }
-  // if (jenjangValue === "") {
-  //   document.getElementById("status").innerHTML = "Jenjang Tidak Boleh Kosong!";
-  //   cek = false; // Stop further execution if the form is not valid
-  // }
-
   if (namaValue === "") {
     document.getElementById("status").innerHTML = "Nama Tidak Boleh Kosong!";
-    return;
+    cek = false;
   }
-  if (kadjurusanValue === "" || jenjangValue === "") {
-    document.getElementById("status").innerHTML = "Tolong Lengkapi Semua Kolom!";
-    return; // Stop further execution if the form is not valid
+  if (kadjurusanValue === "") {
+    document.getElementById("status").innerHTML = "Jurusan Tidak Boleh Kosong!";
+    cek = false; // Stop further execution if the form is not valid
   }
+  if (jenjangValue === "") {
+    document.getElementById("status").innerHTML = "Jenjang Tidak Boleh Kosong!";
+    cek = false; // Stop further execution if the form is not valid
+  }
+
+  // if (namaValue === "") {
+  //   document.getElementById("status").innerHTML = "Nama Tidak Boleh Kosong!";
+  //   return;
+  // }
+  // if (kadjurusanValue === "" || jenjangValue === "") {
+  //   document.getElementById("status").innerHTML = "Tolong Lengkapi Semua Kolom!";
+  //   return; // Stop further execution if the form is not valid
+  // }
 
   let data = {
     kdjurusan: kadjurusanValue,
