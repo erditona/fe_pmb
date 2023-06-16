b4882a0d35e44eaeeb24da82cf03709c1c1f39d2
erditona <erditonaushaadam@gmail.com>
2023-06-16 09:36:00 +0700

    test

--- js/config/url-post-jurusan.js
+++ js/config/url-post-jurusan.js
@@ -7,5 +7,10 @@ export function AmbilResponse(result) {
   if (responseMessageElement) {
     responseMessageElement.textContent = result.message;
   }
-  window.location.reload(); //reload halaman setelah klik ok pada alert
+  // window.location.reload(); //reload halaman setelah klik ok pada alert
+  // Wait for 3 seconds (adjust the duration as needed)
+  const duration = 3000; // 3 seconds
+  setTimeout(() => {
+    window.location.reload(); // Reload the page after the delay
+  }, duration);
 }
