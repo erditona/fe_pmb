731a4753be14ab2b276cf67fa8e423a80fec7ea8
erditona <erditonaushaadam@gmail.com>
2023-06-16 09:07:02 +0700

    test

--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -2,16 +2,16 @@ import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url-post-jurusan.js";
 
-function pushData() {
-  let data = {
-    kdjurusan: getValue("kdjurusan"),
-    nama: getValue("nama"),
-    jenjang: getValue("jenjang"),
-  };
-  console.log(data);
-  postData(urlPOST, data, AmbilResponse);
-}
-onClick("button", pushData);
+// function pushData() {
+//   let data = {
+//     kdjurusan: getValue("kdjurusan"),
+//     nama: getValue("nama"),
+//     jenjang: getValue("jenjang"),
+//   };
+//   console.log(data);
+//   postData(urlPOST, data, AmbilResponse);
+// }
+// onClick("button", pushData);
 
 // function pushData() {
 //   if (kdjurusan || nama || jenjang) {
