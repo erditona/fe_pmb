17ddfbad4c14ec220d889d87df66d4af3cc57274
erditona <erditonaushaadam@gmail.com>
2023-06-16 08:32:54 +0700

    test

--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -2,24 +2,38 @@ import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url-post-jurusan.js";
 
+// function pushData() {
+//   let data = {
+//     kdjurusan: getValue("kdjurusan"),
+//     nama: getValue("nama"),
+//     jenjang: getValue("jenjang"),
+//   };
+//   console.log(data);
+//   postData(urlPOST, data, AmbilResponse);
+// }
+
+// onClick("button", pushData);
+
 function pushData() {
+  // Get the form values
+  let kdjurusanValue = getValue("kdjurusan");
+  let namaValue = getValue("nama");
+  let jenjangValue = getValue("jenjang");
+
+  // Perform form validation
+  if (kdjurusanValue === "" || namaValue === "" || jenjangValue === "") {
+    console.log("Lengkapi Semua Kolom");
+    return; // Stop further execution if the form is not valid
+  }
+
   let data = {
-    kdjurusan: getValue("kdjurusan"),
-    nama: getValue("nama"),
-    jenjang: getValue("jenjang"),
+    kdjurusan: kdjurusanValue,
+    nama: namaValue,
+    jenjang: jenjangValue,
   };
+
   console.log(data);
   postData(urlPOST, data, AmbilResponse);
 }
 
-function validateForm() {
-  let nama = document.forms["FtJurusan"]["nama"].value;
-  if (nama == "") {
-    alert("Name must be filled out");
-    return false;
-  } else {
-    pushData();
-  }
-}
-
-onClick("button", validateForm);
+onClick("button", pushData);
