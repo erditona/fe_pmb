3017581523a8459c3363d16876afaf2e870ce9ec
erditona <erditonaushaadam@gmail.com>
2023-06-16 09:18:57 +0700

    test

--- js/config/url-post-jurusan.js
+++ js/config/url-post-jurusan.js
@@ -2,6 +2,10 @@ export let urlPOST = "https://ws-dito.herokuapp.com/ins-jurusan";
 
 export function AmbilResponse(result) {
   console.log(result); //menampilkan response API pada console
-  alert(result.message); //menampilkan response API pada alert
+  // alert(result.message); //menampilkan response API pada alert
+  let responseMessageElement = document.getElementById("status");
+  if (responseMessageElement) {
+    responseMessageElement.textContent = result.message;
+  }
   window.location.reload(); //reload halaman setelah klik ok pada alert
 }
--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -2,32 +2,6 @@ import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url-post-jurusan.js";
 
-// function pushData() {
-//   let data = {
-//     kdjurusan: getValue("kdjurusan"),
-//     nama: getValue("nama"),
-//     jenjang: getValue("jenjang"),
-//   };
-//   console.log(data);
-//   postData(urlPOST, data, AmbilResponse);
-// }
-// onClick("button", pushData);
-
-// function pushData() {
-//   if (kdjurusan || nama || jenjang) {
-//     let data = {
-//       kdjurusan: getValue("kdjurusan"),
-//       nama: getValue("nama"),
-//       jenjang: getValue("jenjang"),
-//     };
-//     console.log(data);
-//     postData(urlPOST, data, AmbilResponse);
-//   } else {
-//     document.getElementById("status").innerHTML = "Lengkasi Semua Kolom!";
-//   }
-// }
-// onClick("button", pushData);
-
 function pushData() {
   // Get the form values
   let kadjurusanValue = getValue("kdjurusan");
