1e5f8f250d104ddc7bae00f17bae65fdaf636f2c
erditona <erditonaushaadam@gmail.com>
2023-06-16 16:54:15 +0700

    push

--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -9,31 +9,33 @@ function pushData() {
   let jenjangValue = getValue("jenjang");
 
   // Perform form validation
-  if ((namaValue === "") | (kadjurusanValue === "") | (jenjangValue === "")) {
-    document.getElementById("status").innerHTML = "Data Tidak Boleh Kosong!";
-    return;
-  }
+  let cek = true;
+  // if ((namaValue === "") | (kadjurusanValue === "") | (jenjangValue === "")) {
+  //   document.getElementById("status").innerHTML = "Data Tidak Boleh Kosong!";
+  //   return;
+  // }
   if (namaValue === "") {
     document.getElementById("status").innerHTML = "Nama Tidak Boleh Kosong!";
-    return;
+    cek = false;
   }
   if (kadjurusanValue === "") {
     document.getElementById("status").innerHTML = "Jurusan Tidak Boleh Kosong!";
-    return; // Stop further execution if the form is not valid
+    cek = false; // Stop further execution if the form is not valid
   }
   if (jenjangValue === "") {
     document.getElementById("status").innerHTML = "Jenjang Tidak Boleh Kosong!";
-    return; // Stop further execution if the form is not valid
+    cek = false; // Stop further execution if the form is not valid
+  }
+  if (cek) {
+    let data = {
+      kdjurusan: kadjurusanValue,
+      nama: namaValue,
+      jenjang: jenjangValue,
+    };
+
+    console.log(data);
+    postData(urlPOST, data, AmbilResponse);
   }
 
-  let data = {
-    kdjurusan: kadjurusanValue,
-    nama: namaValue,
-    jenjang: jenjangValue,
-  };
-
-  console.log(data);
-  postData(urlPOST, data, AmbilResponse);
+  onClick("button", pushData);
 }
-
-onClick("button", pushData);
