f9f4a1a55ed3e6abf62e3937f81398b72805b84a
erditona <erditonaushaadam@gmail.com>
2023-06-17 16:01:56 +0700

    fix

--- js/controller/post-sekolah.js
+++ js/controller/post-sekolah.js
@@ -4,13 +4,13 @@ import { urlPOST, AmbilResponse } from "../config/url-post-sekolah.js";
 
 function pushData() {
   // Get the form values
-  let kdsekolahValue = getValue("kdsekolah");
+  let kdsekolahValue = parseInt(getValue("kdsekolah"));
   let namaValue = getValue("nama");
   let phoneNumberValue = getValue("phone_number");
   let alamatValue = getValue("alamat");
 
   // Perform form validation
-  if (kdsekolahValue === "" || namaValue === "" || phoneNumberValue === "" || alamatValue === "") {
+  if (isNaN(kdsekolahValue) || kdsekolahValue < 0 || namaValue === "" || phoneNumberValue === "" || alamatValue === "") {
     document.getElementById("status").innerHTML = "Tolong Lengkapi Semua Kolom!";
     return; // Stop further execution if the form is not valid
   }
