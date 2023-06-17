1c72c072a21067adde433cd356b692bf35d7720c
erditona <erditonaushaadam@gmail.com>
2023-06-17 19:31:42 +0700

    add button delete-update sekolah

--- js/temp/table.js
+++ js/temp/table.js
@@ -101,16 +101,13 @@ export let isiTable2 = `
     <div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" id="table-light-1-dropdown">
       <ul class="space-y-2">
         <li>
-          <a href="detail-jurusan.html" class="btn-edit bg-blue-500 hover:bg-blue-700 text-white text-center font-bold py-2 px-4 rounded w-full inline-block">Edit</a>
+          <a href="detail-jurusan.html" class="btn-edit bg-blue-500 hover:bg-blue-700 text-white text-center font-bold py-1 px-2 rounded w-full inline-block">Edit</a>
         </li>
         <li>
-          <button type"button" id="del_button" onclick="deleteData('#IDHAPUS#')" class="btn-delete bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full inline-block">Delete</button>
+          <button type="button" id="del_button" onclick="deleteData('#IDHAPUS#')" class="btn-delete bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded w-full inline-block">Delete</button>
         </li>
       </ul>
     </div>
-
-
-
     </th>
   </tr>
 `;
@@ -154,12 +151,19 @@ export let isiTable3 = `
     <i class="fas fa-ellipsis-v"></i>
     </a>
     <div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48" id="table-light-1-dropdown">
-      <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Update</a
-      ><a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Delete</a>
+      <ul class="space-y-2">
+        <li>
+          <a href="detail-sekolah.html" class="btn-edit bg-blue-500 hover:bg-blue-700 text-white text-center font-bold py-1 px-2 rounded w-full inline-block">Edit</a>
+        </li>
+        <li>
+          <button type="button" id="del_button" onclick="deleteData('#IDHAPUS#')" class="btn-delete bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded w-full inline-block">Delete</button>
+        </li>
+      </ul>
     </div>
     </th>
   </tr>
 `;
+
 //tabelCamaba
 export let isiTable4 = `
   <tr class="h-18 border-b border-coolGray-100">
