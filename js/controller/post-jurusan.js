413c69aa05e25e73dd24b3176152cffbef34ed7c
erditona <erditonaushaadam@gmail.com>
2023-06-17 16:42:29 +0700

    fix

--- js/controller/post-jurusan.js
+++ js/controller/post-jurusan.js
@@ -9,7 +9,7 @@ function pushData() {
   let jenjangValue = document.getElementById("jenjang").value; // Get the selected value from the dropdown
 
   // Perform form validation
-  if ((namaValue === "") & (kadjurusanValue === "") & (jenjangValue === "")) {
+  if (namaValue === "" && kadjurusanValue === "" && jenjangValue === "") {
     document.getElementById("status").innerHTML = "Data Tidak Boleh Kosong!";
     return;
   }
@@ -26,20 +26,14 @@ function pushData() {
     cek = false; // Stop further execution if the form is not valid
   }
 
-  // Additional validation for kdjurusan (4 digits)
-  if (!/^[0-9]{4}$/.test(kadjurusanValue.toString())) {
-    document.getElementById("status").innerHTML = "Format Kode Jurusan Salah! Harap masukkan tepat 4 angka.";
-    return; // Stop further execution if kdsekolah is not valid
+  //validation for kdjurusan
+  if (!/^[A-Za-z0-9]{4}$/.test(kadjurusanValue)) {
+    document.getElementById("status").innerHTML = "Kode Jurusan Harus Terdiri dari 4 Huruf atau Angka!";
+    return; // Stop further execution if the form is not valid
   }
 
-  // if (namaValue === "") {
-  //   document.getElementById("status").innerHTML = "Nama Tidak Boleh Kosong!";
-  //   return;
-  // }
-  // if (kadjurusanValue === "" || jenjangValue === "") {
-  //   document.getElementById("status").innerHTML = "Tolong Lengkapi Semua Kolom!";
-  //   return; // Stop further execution if the form is not valid
-  // }
+  // Clear the status message if all fields are valid
+  document.getElementById("status").innerHTML = "";
 
   let data = {
     kdjurusan: kadjurusanValue,
