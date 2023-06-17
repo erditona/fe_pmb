925cb7b8f1a62e68156ab4ce27d6c740d6094d0e
erditona <erditonaushaadam@gmail.com>
2023-06-17 11:19:42 +0700

    test

--- js/fetch-camaba.js
+++ js/fetch-camaba.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { isiTableCamaba } from "./controller/get-camaba.js";
 
-import { urlAPICamaba } from "./config/url.js";
-get(urlAPICamaba, isiTableCamaba);
+import { urlAPI } from "./config/url.js";
+get(urlAPI, isiTableCamaba);
