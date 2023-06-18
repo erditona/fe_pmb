7c2f2884d9499571b4f425b406df5eab38d2aa2f
erditona <erditonaushaadam@gmail.com>
2023-06-18 22:00:21 +0700

    t

--- js/controller/get-pendaftaran.js
+++ js/controller/get-pendaftaran.js
@@ -1,30 +1,73 @@
+// import { addInner } from "https://bukulapak.github.io/element/process.js";
+// import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+// import { isiTable } from "../temp/table.js";
+
+// //pendaftaran
+// var MyvarPendaftaran = {};
+// export function isiTablePendaftaran(results) {
+//   results.reverse();
+//   MyvarPendaftaran.length = results.length;
+//   results.forEach(isiRow);
+//   console.log(results);
+// }
+// function isiRow(value) {
+//   document.getElementById("jmlpendaftar").innerHTML = "" + MyvarPendaftaran.length + " Data";
+//   let content = isiTable
+//     .replace("#KDPENDAFTAR#", value.kdpendaftar)
+//     .replace("#NAMA#", value.biodata.nama)
+//     .replace("#NOHP#", value.biodata.phone_number)
+//     .replace("#SEKOLAH#", value.asalsekolah.nama)
+//     .replace("#NOHPSEKOLAH#", value.asalsekolah.phone_number)
+//     .replace("#JURUSAN#", value.jurusan.nama)
+//     .replace("#JENJANG#", value.jurusan.jenjang)
+//     .replace("#JALUR#", value.jalur)
+//     .replace("#ALULBI#", value.alulbi)
+//     .replace("#ALJURUSAN#", value.aljurusan)
+//     .replace("#IDHAPUS#", value._id)
+//     .replace("#WARNA#", getRandomColor())
+//     .replace(/#WARNALOGO#/g, getRandomColorName());
+//   addInner("iniTabel", content);
+// }
+
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTable } from "../temp/table.js";
 
-//pendaftaran
+// Pendaftaran
 var MyvarPendaftaran = {};
+
 export function isiTablePendaftaran(results) {
   results.reverse();
   MyvarPendaftaran.length = results.length;
   results.forEach(isiRow);
   console.log(results);
 }
-function isiRow(value) {
+
+async function isiRow(value) {
   document.getElementById("jmlpendaftar").innerHTML = "" + MyvarPendaftaran.length + " Data";
+
+  // Mendapatkan data sekolah berdasarkan ID
+  const sekolahResponse = await fetch(`https://ws-dito.herokuapp.com/sekolah/${value.asalsekolah}`);
+  const sekolahData = await sekolahResponse.json();
+
+  // Mendapatkan data jurusan berdasarkan ID
+  const jurusanResponse = await fetch(`https://ws-dito.herokuapp.com/jurusan/${value.jurusan}`);
+  const jurusanData = await jurusanResponse.json();
+
   let content = isiTable
     .replace("#KDPENDAFTAR#", value.kdpendaftar)
     .replace("#NAMA#", value.biodata.nama)
     .replace("#NOHP#", value.biodata.phone_number)
-    .replace("#SEKOLAH#", value.asalsekolah.nama)
-    .replace("#NOHPSEKOLAH#", value.asalsekolah.phone_number)
-    .replace("#JURUSAN#", value.jurusan.nama)
-    .replace("#JENJANG#", value.jurusan.jenjang)
+    .replace("#SEKOLAH#", sekolahData.nama)
+    .replace("#NOHPSEKOLAH#", sekolahData.phone_number)
+    .replace("#JURUSAN#", jurusanData.nama)
+    .replace("#JENJANG#", jurusanData.jenjang)
     .replace("#JALUR#", value.jalur)
     .replace("#ALULBI#", value.alulbi)
     .replace("#ALJURUSAN#", value.aljurusan)
     .replace("#IDHAPUS#", value._id)
     .replace("#WARNA#", getRandomColor())
     .replace(/#WARNALOGO#/g, getRandomColorName());
+
   addInner("iniTabel", content);
 }
