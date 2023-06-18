2fad7548121a47a91fe23563894290565cc5c4b0
erditona <erditonaushaadam@gmail.com>
2023-06-18 21:04:26 +0700

    test

--- js/controller/post-pendaftaran.js
+++ js/controller/post-pendaftaran.js
@@ -1,3 +1,35 @@
+// Fetch data jurusan from the API for Pilih Jurusan
+fetch("https://ws-dito.herokuapp.com/jurusan")
+  .then((response) => response.json())
+  .then((data) => {
+    const jurusanDropdown = document.getElementById("jurusan");
+
+    // Generate options for each data item
+    data.forEach((item) => {
+      const option = document.createElement("option");
+      option.value = item._id;
+      option.text = item.nama;
+      jurusanDropdown.appendChild(option);
+    });
+  })
+  .catch((error) => console.log(error));
+
+// Fetch data sekolah from the API for Pilih sekolah
+fetch("https://ws-dito.herokuapp.com/sekolah")
+  .then((response) => response.json())
+  .then((data) => {
+    const asalSekolahDropdown = document.getElementById("asalsekolah");
+
+    // Generate options for each data item
+    data.forEach((item) => {
+      const option = document.createElement("option");
+      option.value = item._id;
+      option.text = item.nama;
+      asalSekolahDropdown.appendChild(option);
+    });
+  })
+  .catch((error) => console.log(error));
+
 import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url-post-pendaftaran.js";
