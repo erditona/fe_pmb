91f9f43eb5b5237cb9ec01068d468df790f9fca8
erditona <erditonaushaadam@gmail.com>
2023-06-18 21:33:00 +0700

    t

--- js/controller/post-pendaftaran.js
+++ js/controller/post-pendaftaran.js
@@ -1,79 +1,44 @@
-import { postData } from "https://bukulapak.github.io/api/process.js";
-import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPOST, AmbilResponse } from "../config/url-post-pendaftaran.js";
-
-function fetchData() {
-  fetch("https://ws-dito.herokuapp.com/sekolah")
-    .then((response) => response.json())
-    .then((data) => {
-      const jurusanDropdown = document.getElementById("jurusan");
-      const sekolahDropdown = document.getElementById("asalsekolah");
-
-      data.forEach((item) => {
-        if (item.jenis === "jurusan") {
-          const option = document.createElement("option");
-          option.value = item._id;
-          option.text = item.nama;
-          jurusanDropdown.appendChild(option);
-        } else if (item.jenis === "sekolah") {
-          const option = document.createElement("option");
-          option.value = item._id;
-          option.text = item.nama;
-          sekolahDropdown.appendChild(option);
-        }
-      });
-    })
-    .catch((error) => console.log(error));
-}
-
-fetchData();
-
 function pushData() {
-  const kdpendaftaranValue = parseInt(getValue("kdpendaftaran"));
-  const ktpValue = parseInt(getValue("ktp"));
-  const namaValue = getValue("nama");
-  const phoneValue = getValue("phone_number");
-  const alamatValue = getValue("alamat");
-  const asalSekolahValue = getValue("asalsekolah");
-  const jurusanValue = getValue("jurusan");
-  const jalurValue = getValue("jalur");
-  const alasanULBIValue = getValue("alulbi");
-  const alasanJurusanValue = getValue("aljurusan");
+  // Get the form values
+  let kdpendaftaranValue = parseInt(getValue("kdpendaftaran"));
+  let ktpValue = parseInt(getValue("ktp"));
+  let namaValue = getValue("nama");
+  let phoneValue = getValue("phone_number");
+  let alamatValue = getValue("alamat");
+  let asalSekolahValue = getValue("asalsekolah").toString();
+  let jurusanValue = getValue("jurusan").toString();
+  let jalurValue = getValue("jalur");
+  let alasanULBIValue = getValue("alulbi");
+  let alasanJurusanValue = getValue("aljurusan");
 
-  if (
-    isNaN(kdpendaftaranValue) ||
-    isNaN(ktpValue) ||
-    namaValue === "" ||
-    phoneValue === "" ||
-    alamatValue === "" ||
-    asalSekolahValue === "" ||
-    jurusanValue === "" ||
-    jalurValue === "" ||
-    alasanULBIValue === "" ||
-    alasanJurusanValue === ""
-  ) {
+  // Perform form validation
+  if (ktpValue === "" || namaValue === "" || phoneValue === "" || alamatValue === "" || asalSekolahValue === "" || jurusanValue === "" || jalurValue === "" || alasanULBIValue === "" || alasanJurusanValue === "") {
     document.getElementById("status").textContent = "Data tidak boleh kosong!";
     return;
   }
 
+  // Validate KTP Number
   if (!/^\d{16}$/.test(ktpValue)) {
     document.getElementById("status").textContent = "Nomor KTP harus terdiri dari 16 digit angka.";
     return;
   }
 
+  // Clear the status message if all fields are valid
   document.getElementById("status").textContent = "";
 
-  const data = {
-    kdpendaftaran: kdpendaftaranValue,
-    ktp: ktpValue,
-    nama: namaValue,
-    phone_number: phoneValue,
-    alamat: alamatValue,
-    asalSekolah: asalSekolahValue,
+  let data = {
+    kdpendaftar: kdpendaftaranValue,
+    biodata: {
+      ktp: ktpValue,
+      nama: namaValue,
+      phone_number: phoneValue,
+      alamat: alamatValue,
+    },
+    asalsekolah: asalSekolahValue,
     jurusan: jurusanValue,
     jalur: jalurValue,
-    alasanULBI: alasanULBIValue,
-    alasanJurusan: alasanJurusanValue,
+    alulbi: alasanULBIValue,
+    aljurusan: alasanJurusanValue,
   };
 
   console.log(data);
