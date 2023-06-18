ffdc8413f42de29db1ed1f08b0f2cac372a4e030
erditona <erditonaushaadam@gmail.com>
2023-06-18 22:57:54 +0700

    y

--- js/controller/get-pendaftaran.js
+++ js/controller/get-pendaftaran.js
@@ -6,7 +6,7 @@ import { isiTable } from "../temp/table.js";
 var MyvarPendaftaran = {};
 
 export function isiTablePendaftaran(results) {
-  results.sort((a, b) => new Date(a.created_at) - new Date(b.created_at)); // Sort the array based on created_at in ascending order
+  results.reverse();
   MyvarPendaftaran.length = results.length;
   results.forEach(isiRow);
   console.log(results);
