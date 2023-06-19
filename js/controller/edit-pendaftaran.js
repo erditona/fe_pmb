f2fa813c0504f4562cd9959f765324f2c0e6dfcf
erditona <erditonaushaadam@gmail.com>
2023-06-19 19:44:53 +0700

    test

--- js/controller/edit-pendaftaran.js
+++ js/controller/edit-pendaftaran.js
@@ -1,6 +1,38 @@
+// export function isiData(results) {
+//   const inputMapping = [
+//     { id: "kdpendaftaran", path: "kdpendaftar" },
+//     { id: "ktp", path: "biodata.ktp" },
+//     { id: "nama", path: "biodata.nama" },
+//     { id: "phone_number", path: "biodata.phone_number" },
+//     { id: "alamat", path: "biodata.alamat" },
+//     { id: "asalsekolah", path: "asalsekolah.nama" },
+//     { id: "jurusan", path: "jurusan.nama" },
+//     { id: "jalur", path: "jalur" },
+//     { id: "alulbi", path: "alulbi" },
+//     { id: "aljurusan", path: "aljurusan" },
+//   ];
+
+//   inputMapping.forEach(({ id, path, index, property }) => {
+//     const inputElement = document.getElementById(id);
+//     const value = getNestedValue(results, path, index, property);
+//     inputElement.value = value;
+//   });
+// }
+
+// function getNestedValue(obj, path, index, property) {
+//   const value = path.split(".").reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
+//   // console.log(`Value at path ${path}:`, value);
+
+//   if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
+//     return value[index][property];
+//   }
+
+//   return value;
+// }
+
 export function isiData(results) {
   const inputMapping = [
-    { id: "kdpendaftaran", path: "kdpendaftar" },
+    { id: "kdpendaftaran", path: "kdpendaftaran" },
     { id: "ktp", path: "biodata.ktp" },
     { id: "nama", path: "biodata.nama" },
     { id: "phone_number", path: "biodata.phone_number" },
@@ -12,19 +44,24 @@ export function isiData(results) {
     { id: "aljurusan", path: "aljurusan" },
   ];
 
-  inputMapping.forEach(({ id, path, index, property }) => {
+  inputMapping.forEach(({ id, path }) => {
     const inputElement = document.getElementById(id);
-    const value = getNestedValue(results, path, index, property);
+    const value = getNestedValue(results, path);
     inputElement.value = value;
   });
 }
 
-function getNestedValue(obj, path, index, property) {
-  const value = path.split(".").reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
-  // console.log(`Value at path ${path}:`, value);
+function getNestedValue(obj, path) {
+  const keys = path.split(".");
+  let value = obj;
 
-  if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
-    return value[index][property];
+  for (const key of keys) {
+    if (value && value.hasOwnProperty(key)) {
+      value = value[key];
+    } else {
+      value = "";
+      break;
+    }
   }
 
   return value;
