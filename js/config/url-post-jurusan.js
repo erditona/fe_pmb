e1d238c46440dc051ec2a1f08e0066e26b6c6354
erditona <erditonaushaadam@gmail.com>
2023-06-19 16:14:46 +0700

    add sweet alert jurusan

--- js/config/url-post-jurusan.js
+++ js/config/url-post-jurusan.js
@@ -1,16 +1,17 @@
 export let urlPOST = "https://ws-dito.herokuapp.com/ins-jurusan";
 
 export function AmbilResponse(result) {
-  console.log(result); //menampilkan response API pada console
-  // alert(result.message); //menampilkan response API pada alert
-  let responseMessageElement = document.getElementById("status");
-  if (responseMessageElement) {
-    responseMessageElement.textContent = result.message;
-  }
-  // window.location.reload(); //reload halaman setelah klik ok pada alert
-  // Wait for 3 seconds (adjust the duration as needed)
-  const duration = 3000; // 3 seconds
-  setTimeout(() => {
-    window.location.reload(); // Reload the page after the delay
-  }, duration);
+  console.log(result); // Display API response in the console
+
+  Swal.fire({
+    title: "Notification",
+    text: result.message,
+    icon: "info",
+    confirmButtonColor: "#3085d6",
+    confirmButtonText: "OK",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.reload(); // Reload the page after clicking OK
+    }
+  });
 }
