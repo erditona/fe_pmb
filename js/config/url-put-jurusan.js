fae1de18663dfb9ae9c54a7631189c837b5cc4a2
erditona <erditonaushaadam@gmail.com>
2023-06-19 15:22:59 +0700

    fix alert

--- js/config/url-put-jurusan.js
+++ js/config/url-put-jurusan.js
@@ -3,8 +3,17 @@ const jurusanId = urlParams.get("jurusanId");
 
 export let urlPUT = "https://ws-dito.herokuapp.com/upd-jurusan/" + jurusanId;
 
-export function AmbilResponse(results) {
-  console.log(results); //menampilkan response API pada console
-  alert(results.message); //menampilkan response API pada alert
-  window.location.href = "table-jurusan.html"; //reload halaman setelah klik ok pada alert
+export function AmbilResponse(result) {
+  console.log(result); //menampilkan response API pada console
+
+  let responseMessageElement = document.getElementById("status");
+  if (responseMessageElement) {
+    responseMessageElement.textContent = result.message;
+  }
+
+  // Wait for 3 seconds (adjust the duration as needed)
+  const duration = 3000; // 3 seconds
+  setTimeout(() => {
+    window.location.href = "table-jurusan.html"; //reload halaman setelah klik ok pada alert
+  }, duration);
 }
