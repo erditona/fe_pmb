62eb96dacedd413b9606eb6d5dc8e0f50b21cfde
erditona <erditonaushaadam@gmail.com>
2023-06-19 20:18:31 +0700

    tes

--- js/controller/edit-pendaftaran.js
+++ js/controller/edit-pendaftaran.js
@@ -1,35 +1,3 @@
-export function isiData(results) {
-  const inputMapping = [
-    { id: "kdpendaftaran", path: "kdpendaftar" },
-    { id: "ktp", path: "biodata.ktp" },
-    { id: "nama", path: "biodata.nama" },
-    { id: "phone_number", path: "biodata.phone_number" },
-    { id: "alamat", path: "biodata.alamat" },
-    { id: "asalsekolah", path: "asalsekolah.nama" },
-    { id: "jurusan", path: "jurusan.nama" },
-    { id: "jalur", path: "jalur" },
-    { id: "alulbi", path: "alulbi" },
-    { id: "aljurusan", path: "aljurusan" },
-  ];
-
-  inputMapping.forEach(({ id, path, index, property }) => {
-    const inputElement = document.getElementById(id);
-    const value = getNestedValue(results, path, index, property);
-    inputElement.value = value;
-  });
-}
-
-function getNestedValue(obj, path, index, property) {
-  const value = path.split(".").reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
-  // console.log(`Value at path ${path}:`, value);
-
-  if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
-    return value[index][property];
-  }
-
-  return value;
-}
-
 // export function isiData(results) {
 //   const inputMapping = [
 //     { id: "kdpendaftaran", path: "kdpendaftar" },
@@ -37,32 +5,64 @@ function getNestedValue(obj, path, index, property) {
 //     { id: "nama", path: "biodata.nama" },
 //     { id: "phone_number", path: "biodata.phone_number" },
 //     { id: "alamat", path: "biodata.alamat" },
-//     { id: "asalsekolah", path: "asalsekolah._id" },
-//     { id: "jurusan", path: "jurusan._id" },
+//     { id: "asalsekolah", path: "asalsekolah.nama" },
+//     { id: "jurusan", path: "jurusan.nama" },
 //     { id: "jalur", path: "jalur" },
 //     { id: "alulbi", path: "alulbi" },
 //     { id: "aljurusan", path: "aljurusan" },
 //   ];
 
-//   inputMapping.forEach(({ id, path }) => {
+//   inputMapping.forEach(({ id, path, index, property }) => {
 //     const inputElement = document.getElementById(id);
-//     const value = getNestedValue(results, path);
+//     const value = getNestedValue(results, path, index, property);
 //     inputElement.value = value;
 //   });
 // }
 
-// function getNestedValue(obj, path) {
-//   const keys = path.split(".");
-//   let value = obj;
+// function getNestedValue(obj, path, index, property) {
+//   const value = path.split(".").reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
+//   // console.log(`Value at path ${path}:`, value);
 
-//   for (const key of keys) {
-//     if (value && value.hasOwnProperty(key)) {
-//       value = value[key];
-//     } else {
-//       value = "";
-//       break;
-//     }
+//   if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
+//     return value[index][property];
 //   }
 
 //   return value;
 // }
+
+export function isiData(results) {
+  const inputMapping = [
+    { id: "kdpendaftaran", path: "kdpendaftar" },
+    { id: "ktp", path: "biodata.ktp" },
+    { id: "nama", path: "biodata.nama" },
+    { id: "phone_number", path: "biodata.phone_number" },
+    { id: "alamat", path: "biodata.alamat" },
+    { id: "asalsekolah", path: "asalsekolah._id" },
+    { id: "jurusan", path: "jurusan._id" },
+    { id: "jalur", path: "jalur" },
+    { id: "alulbi", path: "alulbi" },
+    { id: "aljurusan", path: "aljurusan" },
+  ];
+
+  inputMapping.forEach(({ id, path }) => {
+    const inputElement = document.getElementById(id);
+    const value = getNestedValue(results, path);
+    inputElement.value = value;
+  });
+}
+
+function getNestedValue(obj, path) {
+  const keys = path.split(".");
+  let value = obj;
+
+  for (const key of keys) {
+    if (value && value.hasOwnProperty(key)) {
+      value = value[key];
+    } else {
+      value = "";
+      break;
+    }
+  }
+
+  return value;
+}
