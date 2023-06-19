226257afbd80ffc7a82aa1825ceecf2b0e6a43fc
erditona <erditonaushaadam@gmail.com>
2023-06-19 10:38:39 +0700

    tes

--- js/controller/post-pendaftaran.js
+++ js/controller/post-pendaftaran.js
@@ -1,179 +1,66 @@
-// import { postData } from "https://bukulapak.github.io/api/process.js";
-// import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-// import { urlPOST, AmbilResponse } from "../config/url-post-pendaftaran.js";
-
-// async function getSchoolData(schoolId) {
-//   // Fetch school data based on the ID (replace with your API endpoint)
-//   const response = await fetch(`https://ws-dito.herokuapp.com/sekolah/${schoolId}`);
-//   if (response.ok) {
-//     return response.json();
-//   } else {
-//     throw new Error("Failed to fetch school data.");
-//   }
-// }
-
-// async function getMajorData(majorId) {
-//   // Fetch major data based on the ID (replace with your API endpoint)
-//   const response = await fetch(`https://ws-dito.herokuapp.com/jurusan/${majorId}`);
-//   if (response.ok) {
-//     return response.json();
-//   } else {
-//     throw new Error("Failed to fetch major data.");
-//   }
-// }
-
-// function pushData() {
-//   // Get the form values
-//   let kdpendaftaranValue = parseInt(getValue("kdpendaftaran"));
-//   let ktpValue = parseInt(getValue("ktp"));
-//   let namaValue = getValue("nama");
-//   let phoneValue = getValue("phone_number");
-//   let alamatValue = getValue("alamat");
-//   let asalSekolahValue = getValue("asalsekolah"); // Get user input for asalsekolah
-//   let jurusanValue = getValue("jurusan"); // Get user input for jurusan
-//   let jalurValue = getValue("jalur");
-//   let alasanULBIValue = getValue("alulbi");
-//   let alasanJurusanValue = getValue("aljurusan");
-
-//   // Perform form validation
-//   if (ktpValue === "" || namaValue === "" || phoneValue === "" || alamatValue === "" || asalSekolahValue === "" || jurusanValue === "" || jalurValue === "" || alasanULBIValue === "" || alasanJurusanValue === "") {
-//     // Jika ada field yang kosong, tampilkan pesan error
-//     document.getElementById("status").textContent = "Data tidak boleh kosong!";
-//     return;
-//   }
-
-//   if (!/^\d{16}$/.test(ktpValue)) {
-//     // Jika Nomor KTP tidak valid, tampilkan pesan error
-//     document.getElementById("status").textContent = "Nomor KTP harus terdiri dari 16 digit angka.";
-//     return;
-//   }
-
-//   if (!/^0\d{9,12}$/.test(phoneValue)) {
-//     // Jika Nomor Telepon tidak valid, tampilkan pesan error
-//     document.getElementById("status").textContent = "Nomor Telepon harus berawalan 0 dan terdiri dari 10 hingga 13 digit angka.";
-//     return;
-//   }
-
-//   // Clear the status message if all fields are valid
-//   document.getElementById("status").textContent = "";
-
-//   // Ambil data sekolah dan data jurusan secara bersamaan
-//   Promise.all([getSchoolData(asalSekolahValue), getMajorData(jurusanValue)])
-//     .then(([schoolData, majorData]) => {
-//       // Ekstrak nilai-nilai yang diperlukan dari data yang diambil
-//       let asalSekolahText = schoolData.nama;
-//       let phoneSekolahValue = schoolData.phone_number;
-//       let alamatSekolahValue = schoolData.alamat;
-
-//       let jurusanText = majorData.nama;
-//       let kdjurusanValue = majorData.kdjurusan;
-//       let jenjangValue = majorData.jenjang;
-
-//       // Bangun objek data
-//       let data = {
-//         kdpendaftar: kdpendaftaranValue,
-//         biodata: {
-//           ktp: ktpValue,
-//           nama: namaValue,
-//           phone_number: phoneValue,
-//           alamat: alamatValue,
-//         },
-//         asalsekolah: {
-//           _id: asalSekolahValue,
-//           nama: asalSekolahText,
-//           phone_number: phoneSekolahValue,
-//           alamat: alamatSekolahValue,
-//         },
-//         jurusan: {
-//           _id: jurusanValue,
-//           kdjurusan: kdjurusanValue,
-//           nama: jurusanText,
-//           jenjang: jenjangValue,
-//         },
-//         jalur: jalurValue,
-//         alulbi: alasanULBIValue,
-//         aljurusan: alasanJurusanValue,
-//       };
-
-//       console.log(data);
-//       postData(urlPOST, data, AmbilResponse);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       document.getElementById("status").textContent = "Failed to fetch data.";
-//     });
-// }
-
-// onClick("button", pushData);
-
 import { postData } from "https://bukulapak.github.io/api/process.js";
-import { onClick, getValue, setValue } from "https://bukulapak.github.io/element/process.js";
+import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url-post-pendaftaran.js";
 
-async function isKodePendaftaranTerdaftar(kodePendaftaran) {
-  try {
-    const response = await fetch(`https://ws-dito.herokuapp.com/pendaftaran/kd/${kodePendaftaran}`);
-    const data = await response.json();
-    return response.ok && data.length > 0;
-  } catch (error) {
-    console.error(error);
-    return false;
+async function getSchoolData(schoolId) {
+  // Fetch school data based on the ID (replace with your API endpoint)
+  const response = await fetch(`https://ws-dito.herokuapp.com/sekolah/${schoolId}`);
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Failed to fetch school data.");
   }
 }
 
-async function generateKodePendaftaran() {
-  var currentDate = new Date();
-  var year = currentDate.getFullYear().toString().substr(-2);
-  var month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-  var day = currentDate.getDate().toString().padStart(2, "0");
-
-  var randomNum = localStorage.getItem("randomNum");
-  if (!randomNum || parseInt(randomNum.substr(0, 6)) < parseInt(day + month + year)) {
-    randomNum = day + month + year + "01";
+async function getMajorData(majorId) {
+  // Fetch major data based on the ID (replace with your API endpoint)
+  const response = await fetch(`https://ws-dito.herokuapp.com/jurusan/${majorId}`);
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Failed to fetch major data.");
   }
-
-  var kodePendaftaran = parseInt(randomNum);
-
-  const isTerdaftar = await isKodePendaftaranTerdaftar(kodePendaftaran);
-  if (isTerdaftar) {
-    var increment = parseInt(randomNum.substr(-2)) + 1;
-    randomNum = randomNum.substr(0, randomNum.length - 2) + increment.toString().padStart(2, "0");
-    kodePendaftaran = parseInt(randomNum);
-  }
-
-  return kodePendaftaran;
 }
 
 function pushData() {
+  // Get the form values
+  let kdpendaftaranValue = parseInt(getValue("kdpendaftaran"));
   let ktpValue = parseInt(getValue("ktp"));
   let namaValue = getValue("nama");
   let phoneValue = getValue("phone_number");
   let alamatValue = getValue("alamat");
-  let asalSekolahValue = getValue("asalsekolah");
-  let jurusanValue = getValue("jurusan");
+  let asalSekolahValue = getValue("asalsekolah"); // Get user input for asalsekolah
+  let jurusanValue = getValue("jurusan"); // Get user input for jurusan
   let jalurValue = getValue("jalur");
   let alasanULBIValue = getValue("alulbi");
   let alasanJurusanValue = getValue("aljurusan");
 
+  // Perform form validation
   if (ktpValue === "" || namaValue === "" || phoneValue === "" || alamatValue === "" || asalSekolahValue === "" || jurusanValue === "" || jalurValue === "" || alasanULBIValue === "" || alasanJurusanValue === "") {
+    // Jika ada field yang kosong, tampilkan pesan error
     document.getElementById("status").textContent = "Data tidak boleh kosong!";
     return;
   }
 
   if (!/^\d{16}$/.test(ktpValue)) {
+    // Jika Nomor KTP tidak valid, tampilkan pesan error
     document.getElementById("status").textContent = "Nomor KTP harus terdiri dari 16 digit angka.";
     return;
   }
 
   if (!/^0\d{9,12}$/.test(phoneValue)) {
+    // Jika Nomor Telepon tidak valid, tampilkan pesan error
     document.getElementById("status").textContent = "Nomor Telepon harus berawalan 0 dan terdiri dari 10 hingga 13 digit angka.";
     return;
   }
 
+  // Clear the status message if all fields are valid
   document.getElementById("status").textContent = "";
 
+  // Ambil data sekolah dan data jurusan secara bersamaan
   Promise.all([getSchoolData(asalSekolahValue), getMajorData(jurusanValue)])
     .then(([schoolData, majorData]) => {
+      // Ekstrak nilai-nilai yang diperlukan dari data yang diambil
       let asalSekolahText = schoolData.nama;
       let phoneSekolahValue = schoolData.phone_number;
       let alamatSekolahValue = schoolData.alamat;
@@ -182,42 +69,34 @@ function pushData() {
       let kdjurusanValue = majorData.kdjurusan;
       let jenjangValue = majorData.jenjang;
 
-      generateKodePendaftaran()
-        .then((kdpendaftaranValue) => {
-          setValue("kdpendaftaran", kdpendaftaranValue); // Set the generated kode pendaftaran
+      // Bangun objek data
+      let data = {
+        kdpendaftar: kdpendaftaranValue,
+        biodata: {
+          ktp: ktpValue,
+          nama: namaValue,
+          phone_number: phoneValue,
+          alamat: alamatValue,
+        },
+        asalsekolah: {
+          _id: asalSekolahValue,
+          nama: asalSekolahText,
+          phone_number: phoneSekolahValue,
+          alamat: alamatSekolahValue,
+        },
+        jurusan: {
+          _id: jurusanValue,
+          kdjurusan: kdjurusanValue,
+          nama: jurusanText,
+          jenjang: jenjangValue,
+        },
+        jalur: jalurValue,
+        alulbi: alasanULBIValue,
+        aljurusan: alasanJurusanValue,
+      };
 
-          let data = {
-            kdpendaftar: kdpendaftaranValue,
-            biodata: {
-              ktp: ktpValue,
-              nama: namaValue,
-              phone_number: phoneValue,
-              alamat: alamatValue,
-            },
-            asalsekolah: {
-              _id: asalSekolahValue,
-              nama: asalSekolahText,
-              phone_number: phoneSekolahValue,
-              alamat: alamatSekolahValue,
-            },
-            jurusan: {
-              _id: jurusanValue,
-              kdjurusan: kdjurusanValue,
-              nama: jurusanText,
-              jenjang: jenjangValue,
-            },
-            jalur: jalurValue,
-            alulbi: alasanULBIValue,
-            aljurusan: alasanJurusanValue,
-          };
-
-          console.log(data);
-          postData(urlPOST, data, AmbilResponse);
-        })
-        .catch((error) => {
-          console.error(error);
-          document.getElementById("status").textContent = "Failed to generate kode pendaftaran.";
-        });
+      console.log(data);
+      postData(urlPOST, data, AmbilResponse);
     })
     .catch((error) => {
       console.error(error);
--- js/generateKdpendaftaran.js
+++ js/generateKdpendaftaran.js
@@ -1,139 +1,5 @@
-// // Fungsi untuk menghasilkan kode pendaftaran
-// function generateKodePendaftaran() {
-//   // Mendapatkan tanggal sekarang
-//   var currentDate = new Date();
-//   // Mendapatkan dua digit tahun terakhir
-//   var year = currentDate.getFullYear().toString().substr(-2);
-//   // Mendapatkan dua digit bulan
-//   var month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-//   // Mendapatkan dua digit tanggal
-//   var day = currentDate.getDate().toString().padStart(2, "0");
-
-//   // Mengambil angka acak berurutan dari localStorage
-//   var randomNum = localStorage.getItem("randomNum");
-//   // Jika angka acak belum ada atau lebih kecil dari tanggal sekarang, inisialisasi dengan tanggal sekarang
-//   if (!randomNum || parseInt(randomNum.substr(0, 6)) < parseInt(day + month + year)) {
-//     randomNum = day + month + year + "01";
-//   }
-
-//   // Menggabungkan semua bagian untuk membentuk kode pendaftaran
-//   var kodePendaftaran = randomNum;
-//   return kodePendaftaran;
-// }
-
-// // Mendapatkan elemen input kode pendaftaran
-// var kdpendaftaranInput = document.getElementById("kdpendaftaran");
-// // Mengisi nilai input kode pendaftaran dengan kode yang dihasilkan
-// kdpendaftaranInput.value = generateKodePendaftaran();
-
-// // Simpan angka acak berurutan ke localStorage
-// localStorage.setItem("randomNum", kdpendaftaranInput.value);
-
-// async function isKodePendaftaranTerdaftar(kodePendaftaran) {
-//   try {
-//     const response = await fetch(`https://ws-dito.herokuapp.com/pendaftaran?_id=${kodePendaftaran}`);
-//     const data = await response.json();
-//     return response.ok && data.length > 0;
-//   } catch (error) {
-//     console.error(error);
-//     return false;
-//   }
-// }
-
-// // Fungsi untuk menghasilkan kode pendaftaran yang belum terdaftar
-// async function generateKodePendaftaran() {
-//   // Mendapatkan tanggal sekarang
-//   var currentDate = new Date();
-//   // Mendapatkan dua digit tahun terakhir
-//   var year = currentDate.getFullYear().toString().substr(-2);
-//   // Mendapatkan dua digit bulan
-//   var month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-//   // Mendapatkan dua digit tanggal
-//   var day = currentDate.getDate().toString().padStart(2, "0");
-
-//   // Mengambil angka acak berurutan dari localStorage
-//   var randomNum = localStorage.getItem("randomNum");
-//   // Jika angka acak belum ada atau lebih kecil dari tanggal sekarang, inisialisasi dengan tanggal sekarang
-//   if (!randomNum || parseInt(randomNum.substr(0, 6)) < parseInt(day + month + year)) {
-//     randomNum = day + month + year + "01";
-//   }
-
-//   // Menggabungkan semua bagian untuk membentuk kode pendaftaran
-//   var kodePendaftaran = randomNum;
-
-//   // Memeriksa apakah kode pendaftaran sudah terdaftar di API
-//   const isTerdaftar = await isKodePendaftaranTerdaftar(kodePendaftaran);
-//   if (isTerdaftar) {
-//     // Jika kode pendaftaran sudah terdaftar, menghasilkan kode baru dengan increment angka acak
-//     var increment = parseInt(randomNum.substr(-2)) + 1;
-//     randomNum = randomNum.substr(0, randomNum.length - 2) + increment.toString().padStart(2, "0");
-//     kodePendaftaran = randomNum;
-//   }
-
-//   // Simpan angka acak berurutan ke localStorage
-//   localStorage.setItem("randomNum", kodePendaftaran);
-
-//   return kodePendaftaran;
-// }
-
-// // Mendapatkan elemen input kode pendaftaran
-// var kdpendaftaranInput = document.getElementById("kdpendaftaran");
-
-// // Mengisi nilai input kode pendaftaran dengan kode yang dihasilkan
-// generateKodePendaftaran()
-//   .then((kodePendaftaran) => {
-//     kdpendaftaranInput.value = kodePendaftaran;
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
 // Fungsi untuk menghasilkan kode pendaftaran sederhana
-// function generateKodePendaftaran() {
-//   // Mendapatkan tanggal sekarang
-//   var currentDate = new Date();
-//   // Mendapatkan dua digit tahun terakhir
-//   var year = currentDate.getFullYear().toString().substr(-2);
-//   // Mendapatkan dua digit bulan
-//   var month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-//   // Mendapatkan dua digit tanggal
-//   var day = currentDate.getDate().toString().padStart(2, "0");
-
-//   // Mengambil angka acak berurutan dari localStorage
-//   var randomNum = localStorage.getItem("randomNum");
-//   // Jika angka acak belum ada, inisialisasi dengan 1
-//   if (!randomNum) {
-//     randomNum = 1;
-//   } else {
-//     // Jika angka acak sudah ada, tambahkan 1
-//     randomNum = parseInt(randomNum) + 1;
-//   }
-//   // Simpan angka acak berurutan ke localStorage
-//   localStorage.setItem("randomNum", randomNum);
-
-//   // Menggabungkan semua bagian untuk membentuk kode pendaftaran
-//   var kodePendaftaran = day + month + year + randomNum.toString().padStart(2, "0");
-//   return kodePendaftaran;
-// }
-
-// // Mendapatkan elemen input kode pendaftaran
-// var kdpendaftaranInput = document.getElementById("kdpendaftaran");
-// // Mengisi nilai input kode pendaftaran dengan kode yang dihasilkan
-// kdpendaftaranInput.value = generateKodePendaftaran();
-
-async function isKodePendaftaranTerdaftar(kodePendaftaran) {
-  try {
-    const response = await fetch(`https://ws-dito.herokuapp.com/pendaftaran/kd/${kodePendaftaran}`);
-    const data = await response.json();
-    return response.ok && data.length > 0;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
-
-// Fungsi untuk menghasilkan kode pendaftaran yang belum terdaftar
-async function generateKodePendaftaran() {
+function generateKodePendaftaran() {
   // Mendapatkan tanggal sekarang
   var currentDate = new Date();
   // Mendapatkan dua digit tahun terakhir
@@ -145,94 +11,22 @@ async function generateKodePendaftaran() {
 
   // Mengambil angka acak berurutan dari localStorage
   var randomNum = localStorage.getItem("randomNum");
-  // Jika angka acak belum ada atau lebih kecil dari tanggal sekarang, inisialisasi dengan tanggal sekarang
-  if (!randomNum || parseInt(randomNum.substr(0, 6)) < parseInt(day + month + year)) {
-    randomNum = day + month + year + "01";
+  // Jika angka acak belum ada, inisialisasi dengan 1
+  if (!randomNum) {
+    randomNum = 1;
+  } else {
+    // Jika angka acak sudah ada, tambahkan 1
+    randomNum = parseInt(randomNum) + 1;
   }
-
-  // Memeriksa apakah kode pendaftaran sudah terdaftar di API
-  var isTerdaftar = await isKodePendaftaranTerdaftar(randomNum);
-  if (isTerdaftar) {
-    // Jika kode pendaftaran sudah terdaftar, mencari kode pendaftaran yang belum terdaftar
-    var increment = 1;
-    do {
-      var newRandomNum = parseInt(randomNum.substr(-2)) + increment;
-      randomNum = randomNum.substr(0, randomNum.length - 2) + newRandomNum.toString().padStart(2, "0");
-      isTerdaftar = await isKodePendaftaranTerdaftar(randomNum);
-      increment++;
-    } while (isTerdaftar);
-  }
-
   // Simpan angka acak berurutan ke localStorage
   localStorage.setItem("randomNum", randomNum);
 
   // Menggabungkan semua bagian untuk membentuk kode pendaftaran
-  var kodePendaftaran = randomNum;
-
+  var kodePendaftaran = day + month + year + randomNum.toString().padStart(2, "0");
   return kodePendaftaran;
 }
 
 // Mendapatkan elemen input kode pendaftaran
 var kdpendaftaranInput = document.getElementById("kdpendaftaran");
-
 // Mengisi nilai input kode pendaftaran dengan kode yang dihasilkan
-generateKodePendaftaran()
-  .then((kodePendaftaran) => {
-    kdpendaftaranInput.value = kodePendaftaran;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
-// Menyimpan angka acak berurutan ke localStorage setelah insert dilakukan
-document.getElementById("form-pendaftaran").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  var kodePendaftaran = kdpendaftaranInput.value;
-
-  fetch("https://ws-dito.herokuapp.com/pendaftaran", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      kdpendaftar: kodePendaftaran,
-      biodata: {
-        ktp: 1234567890,
-        nama: "John Doe",
-        phone_number: "081234567890",
-        alamat: "Jl. Contoh Alamat",
-      },
-      asalsekolah: {
-        nama: "SMA Contoh",
-        phone_number: "081234567891",
-        alamat: "Jl. Contoh Alamat Sekolah",
-      },
-      jurusan: {
-        kdjurusan: "123",
-        nama: "Contoh Jurusan",
-        jenjang: "S1",
-      },
-      jalur: "Rapot",
-      alulbi: "Rekomendasi",
-      aljurusan: "Keren dan Menarik",
-    }),
-  })
-    .then((response) => {
-      if (response.ok) {
-        // Mengupdate nilai input kode pendaftaran setelah insert dilakukan
-        generateKodePendaftaran()
-          .then((newKodePendaftaran) => {
-            kdpendaftaranInput.value = newKodePendaftaran;
-            // Simpan angka acak berurutan ke localStorage
-            localStorage.setItem("randomNum", newKodePendaftaran);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-});
+kdpendaftaranInput.value = generateKodePendaftaran();
