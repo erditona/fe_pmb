0169269ba2e68e5667bd45db05d4be2ed63c1685
erditona <erditonaushaadam@gmail.com>
2023-06-19 18:39:34 +0700

    add sweet alert for pendaftaran

--- js/controller/delete-pendaftaran.js
+++ js/controller/delete-pendaftaran.js
@@ -2,19 +2,36 @@ function deleteData(IDHAPUS) {
   var pendaftaranId = IDHAPUS;
   var target_url = "https://ws-dito.herokuapp.com/delete-pendaftaran/" + pendaftaranId;
 
-  var confirmation = window.confirm("Apakah Anda yakin ingin menghapus data ini?");
-  if (confirmation) {
-    var requestOptions = {
-      method: "DELETE",
-      redirect: "follow",
-    };
+  Swal.fire({
+    title: "Confirmation",
+    text: "Apakah Anda yakin ingin menghapus data ini?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Ya",
+    cancelButtonText: "Tidak",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      var requestOptions = {
+        method: "DELETE",
+        redirect: "follow",
+      };
 
-    fetch(target_url, requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        window.alert(result.message);
-        location.reload();
-      })
-      .catch((error) => console.log("Error:", error));
-  }
+      fetch(target_url, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          Swal.fire({
+            title: "Notification",
+            text: result.message,
+            icon: "success",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "OK",
+          }).then(() => {
+            location.reload();
+          });
+        })
+        .catch((error) => console.log("Error:", error));
+    }
+  });
 }
