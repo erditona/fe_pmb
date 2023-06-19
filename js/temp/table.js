a4eb70a666d8cb9fbe71bd56eab8718f3a0f38dc
erditona <erditonaushaadam@gmail.com>
2023-06-19 22:50:22 +0700

    merapikan

--- js/temp/table.js
+++ js/temp/table.js
@@ -4,9 +4,6 @@ export let isiTable = `
     <th class="whitespace-nowrap px-4 bg-white text-left">
       <div class="flex items-center -m-2">
         <div class="w-auto p-2">
-          <input class="w-4 h-4 bg-white rounded" type="checkbox" />
-        </div>
-        <div class="w-auto p-2">
             <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
         </div>
         <div class="w-auto p-2">
@@ -47,7 +44,13 @@ export let isiTable = `
         </div>
       </div>
     </th>
-    
+    <th class="whitespace-nowrap px-4 bg-white text-center">
+        <div class="w-auto p-2">
+          <p class="text-xs font-semibold text-coolGray-800">#ALJURUSAN#</p>
+        </div>
+      </div>
+    </th>
+
     <th class="whitespace-nowrap pr-4 bg-white text-sm font-medium text-coolGray-800">
         <a type="button" href="edit-pendaftaran.html?pendaftaranId=#IDEDIT#" class="btn-edit">
             <i class="fas fa-edit mr-1"></i> Edit
@@ -74,9 +77,6 @@ export let isiTable2 = `
   <tr class="h-18 border-b border-coolGray-100">
     <th class="whitespace-nowrap px-4 bg-white text-left">
       <div class="flex items-center -m-2">
-        <div class="w-auto p-2">
-          <input class="w-4 h-4 bg-white rounded" type="checkbox" />
-        </div>
         <div class="w-auto p-2">
             <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
         </div>
@@ -124,9 +124,6 @@ export let isiTable3 = `
   <tr class="h-18 border-b border-coolGray-100">
     <th class="whitespace-nowrap px-4 bg-white text-left">
       <div class="flex items-center -m-2">
-        <div class="w-auto p-2">
-          <input class="w-4 h-4 bg-white rounded" type="checkbox" />
-        </div>
         <div class="w-auto p-2">
             <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
         </div>
@@ -179,9 +176,6 @@ export let isiTable4 = `
   <tr class="h-18 border-b border-coolGray-100">
     <th class="whitespace-nowrap px-4 bg-white text-left">
       <div class="flex items-center -m-2">
-        <div class="w-auto p-2">
-          <input class="w-4 h-4 bg-white rounded" type="checkbox" />
-        </div>
         <div class="w-auto p-2">
             <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
         </div>
