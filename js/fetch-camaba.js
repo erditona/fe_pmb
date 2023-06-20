47319e8f0f4a50ea2bcfffe70974586cdd25e834
erditona <erditonaushaadam@gmail.com>
2023-06-20 16:34:18 +0700

    add  new camaba

--- js/fetch-camaba.js
+++ js/fetch-camaba.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { isiTableCamaba } from "./controller/get-camaba.js";
 
-import { urlAPI } from "./config/url.js";
-get(urlAPI, isiTableCamaba);
+import { urlAPICamaba } from "./config/url.js";
+get(urlAPICamaba, isiTableCamaba);
