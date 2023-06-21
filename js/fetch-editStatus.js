670d06271339a33ec77185c27382a286dcca9f3e
erditona <erditonaushaadam@gmail.com>
2023-06-21 20:26:54 +0700

    test

--- js/fetch-editStatus.js
+++
@@ -1,4 +0,0 @@
-import { get } from "https://bukulapak.github.io/api/process.js";
-import { isiData } from "./controller/edit-pendaftaran.js";
-import { urlFetch } from "./config/url-get-detail-pendaftaran.js";
-get(urlFetch, isiData);
