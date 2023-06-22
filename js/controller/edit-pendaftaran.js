6685a5c3ff853c57dac5e302fbc8b8f7e5c3c52e
erditona <erditonaushaadam@gmail.com>
2023-06-22 22:45:40 +0700

    test

--- js/controller/edit-pendaftaran.js
+++ js/controller/edit-pendaftaran.js
@@ -30,39 +30,31 @@ function getNestedValue(obj, path, index, property) {
   return value;
 }
 
-// export function isiData(results) {
-//   const inputMapping = [
-//     { id: "kdpendaftaran", path: "kdpendaftar" },
-//     { id: "ktp", path: "biodata.ktp" },
-//     { id: "nama", path: "biodata.nama" },
-//     { id: "phone_number", path: "biodata.phone_number" },
-//     { id: "alamat", path: "biodata.alamat" },
-//     { id: "asalsekolah", path: "asalsekolah._id" },
-//     { id: "jurusan", path: "jurusan._id" },
-//     { id: "jalur", path: "jalur" },
-//     { id: "alulbi", path: "alulbi" },
-//     { id: "aljurusan", path: "aljurusan" },
-//   ];
+export function isiData2(results) {
+  const inputMapping = [
+    { id: "kdpendaftaran", path: "kdpendaftar" },
+    { id: "ktp", path: "biodata.ktp" },
+    { id: "nama", path: "biodata.nama" },
+    { id: "statuspendaftar", path: "statuspendaftar" },
+  ];
 
-//   inputMapping.forEach(({ id, path }) => {
-//     const inputElement = document.getElementById(id);
-//     const value = getNestedValue(results, path);
-//     inputElement.value = value;
-//   });
-// }
+  inputMapping.forEach(({ id, path, index, property }) => {
+    const inputElement = document.getElementById(id);
+    const value = getNestedValue2(results, path, index, property);
+    inputElement.value = value;
+  });
+}
 
-// function getNestedValue(obj, path) {
-//   const keys = path.split(".");
-//   let value = obj;
+function getNestedValue2(obj, path, index, property) {
+  const value = path.split(".").reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
+  // console.log(`Value at path ${path}:`, value);
 
-//   for (const key of keys) {
-//     if (value && value.hasOwnProperty(key)) {
-//       value = value[key];
-//     } else {
-//       value = "";
-//       break;
-//     }
-//   }
+  if (Array.isArray(value) && value.length > index && value[index].hasOwnProperty(property)) {
+    return value[index][property];
+  }
+
+  return value;
+}
 
 //   return value;
 // }
