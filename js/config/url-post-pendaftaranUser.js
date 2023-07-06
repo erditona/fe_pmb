8094e02733b242d2d5f1aa9eeb49bb7c20c387ac
erditona <erditonaushaadam@gmail.com>
2023-07-07 01:52:03 +0700

    Merapikan

--- js/config/url-post-pendaftaranUser.js
+++ js/config/url-post-pendaftaranUser.js
@@ -1,3 +1,45 @@
+// export let urlPOST = "https://ws-dito.herokuapp.com/ins-pendaftaran";
+
+// export function AmbilResponse(result) {
+//   console.log(result); // Tampilkan respons API di konsol
+
+//   Swal.fire({
+//     title: "Notifikasi",
+//     text: result.message,
+//     icon: "info",
+//     confirmButtonColor: "#3085d6",
+//     confirmButtonText: "OK",
+//   }).then((swalResult) => {
+//     if (swalResult.isConfirmed) {
+//       document.getElementById("formdaftaruser").style.display = "none";
+//       if (result.returned_kdpendaftar && result.returned_status) {
+//         document.getElementById("berhasilDaftar").innerHTML = `
+//           <div class="card content-center items-center justify-center h-full" style="display: flex; justify-content: center;">
+//             <div class="card-header">
+//               Anda Sudah Terdaftar!
+//             </div>
+//             <div class="card-body">
+//               <p>Kode Pendaftar: ${result.returned_kdpendaftar}</p>
+//               <p>Status Pendaftar: ${result.returned_status}</p>
+//             </div>
+//           </div>
+//         `;
+//       } else {
+//         document.getElementById("berhasilDaftar").innerHTML = `
+//           <div class="card content-center items-center justify-center h-full" style="display: flex; justify-content: center;">
+//             <div class="card-header">
+//               Terjadi Kesalahan
+//             </div>
+//             <div class="card-body">
+//               <p>Gagal memperoleh informasi pendaftaran.</p>
+//             </div>
+//           </div>
+//         `;
+//       }
+//     }
+//   });
+// }
+
 export let urlPOST = "https://ws-dito.herokuapp.com/ins-pendaftaran";
 
 export function AmbilResponse(result) {
@@ -9,19 +51,40 @@ export function AmbilResponse(result) {
     icon: "info",
     confirmButtonColor: "#3085d6",
     confirmButtonText: "OK",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      document.getElementById("formdaftaruser").style.display = 'none';
-      document.getElementById("berhasilDaftar").innerHTML = `
-        <div class="card content-center items-center justify-center h-full" style="display: flex; justify-content: center;">
-          <div class="card-header">
-            Anda Sudah Terdaftar!
+  }).then((swalResult) => {
+    if (swalResult.isConfirmed) {
+      document.getElementById("formdaftaruser").style.display = "none";
+      if (result.returned_kdpendaftar && result.returned_status) {
+        // Menyimpan informasi pendaftaran di local storage
+        const pendaftaran = {
+          returned_kdpendaftar: result.returned_kdpendaftar,
+          returned_status: result.returned_status,
+        };
+        localStorage.setItem("pendaftaran", JSON.stringify(pendaftaran));
+
+        document.getElementById("berhasilDaftar").innerHTML = `
+          <div class="card content-center items-center justify-center h-full" style="display: flex; justify-content: center;">
+            <div class="card-header">
+              Anda Sudah Terdaftar!
+            </div>
+            <div class="card-body">
+              <p>Kode Pendaftar: ${result.returned_kdpendaftar}</p>
+              <p>Status Pendaftar: ${result.returned_status}</p>
+            </div>
           </div>
-         
-        </div>
-      `;
+        `;
+      } else {
+        document.getElementById("berhasilDaftar").innerHTML = `
+          <div class="card content-center items-center justify-center h-full" style="display: flex; justify-content: center;">
+            <div class="card-header">
+              Terjadi Kesalahan
+            </div>
+            <div class="card-body">
+              <p>Gagal memperoleh informasi pendaftaran.</p>
+            </div>
+          </div>
+        `;
+      }
     }
   });
 }
-
-
